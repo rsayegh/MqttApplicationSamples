a4affa6ad87c5365bfdf53b7a7457168bb50dfd3
rsayegh <147048331+rsayegh@users.noreply.github.com>
2025-03-01 09:23:02 +0000

    a

--- scenarios/telemetry/python/telemetry_producer.py
+++ scenarios/telemetry/python/telemetry_producer.py
@@ -214,7 +214,7 @@ def main():
 
             # Generate sample data
             sample_data = {
-                "vehicles": generate_vehicle_data(vehicle_count=1)
+                "data": generate_vehicle_data(vehicle_count=1)
             }
 
             payload = str(sample_data)
